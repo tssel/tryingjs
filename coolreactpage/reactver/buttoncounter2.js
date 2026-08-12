3612af0e0193a9d323c57a7be415ef6e388ea5d1
tssel <177930405+tssel@users.noreply.github.com>
2026-08-12 23:08:03 +0100

    more stuff

--- coolreactpage/reactver/buttoncounter2.js
+++ coolreactpage/reactver/buttoncounter2.js
@@ -1,14 +1,47 @@
+function render() {
+    root.innerHTML = app();
+}
+
+const state = new Proxy(
+    {buttoncounter: 0},
+    {
+        set(target,key,value) {
+            target[key] = value
+            render()
+        }
+    
+});
+
 function app() {
     return `
     <h1>The page</h1>
+    ${ButtonArea()}
+    ${fruits()}
+    `
+};
+
+function ButtonArea() {
+    return `<div>
+    <h2>Button Counter</h2>
+    <p>The Button has been pressed ${state.buttoncounter} times.</p>
+    <button onclick="state.buttoncounter++">Press Button</button>
+    </div>`
+};
+
+function fruit(name) {
+    return `
+    <li>There is a ${name}</li>
     `
 };
 
 function fruits(){
     return `
     <h2>List of fruits:</h2>
+    <ul>
+        ${fruit("apple")}
+        ${fruit("banana")}
     `
 };
 
 const root = document.getElementById("app");
-root.innerHTML = app();
+render()
