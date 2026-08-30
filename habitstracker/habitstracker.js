5b903286bb7bac8ef9e4f555ccef460a7acce57f
tssel <177930405+tssel@users.noreply.github.com>
2026-08-30 18:04:03 +0100

    more stuff

--- habitstracker/habitstracker.js
+++ habitstracker/habitstracker.js
@@ -4,5 +4,40 @@ root.innerHTML = app();
 function app() {
     return `
     <h1>Habits Tracker</h1>
+    <div id="habitviewer"></div>
+    <input id="habitname">
+    <button onclick="addHabit()">Add Habit</button>
     `;
 }
+let habits = [
+    {
+        id: 1,
+        name: "Go to gym",
+        completed: false
+    },
+    {
+        id: 2,
+        name: "Eat healthy",
+        completed: true
+    }
+];
+function fetchHabits() {
+    const habitviewer = document.getElementById("habitviewer");
+    habitviewer.innerHTML = habits
+        .map(habit => `
+        <p>${habit.name} - ${habit.completed ? "not yet completed" : "completed"}</p>
+        `).join("");
+}
+function addHabit() {
+    const input = document.getElementById("habitname");
+    const habitname = input.value.trim();
+    const newHabit = {
+        id: habits.length + 1,
+        name: habitname,
+        completed: false
+    };
+    habits.push(newHabit);
+    input.value = "";
+    fetchHabits();
+}
+fetchHabits();
--- habitstracker/habitstracker.ts
+++ habitstracker/habitstracker.ts
@@ -1,8 +1,55 @@
-const root = document.getElementById("app") as HTMLDivElement
+type habit = {
+    id: number,
+    name: string,
+    completed: boolean
+}
 
+const root = document.getElementById("app") as HTMLDivElement
 root.innerHTML = app()
+
 function app(): string {
     return `
     <h1>Habits Tracker</h1>
+    <div id="habitviewer"></div>
+    <input id="habitname">
+    <button onclick="addHabit()">Add Habit</button>
     `
 }
+
+
+let habits: habit[] = [
+    {
+        id: 1,
+        name: "Go to gym",
+        completed: false
+    },
+    {
+        id: 2,
+        name: "Eat healthy",
+        completed: true
+    }
+]
+
+function fetchHabits(): void {
+    const habitviewer = document.getElementById("habitviewer") as HTMLDivElement
+    habitviewer.innerHTML = habits
+    .map(habit => `
+        <p>${habit.name} - ${habit.completed ? "not yet completed" : "completed"}</p>
+        `
+    ).join("")
+}
+
+function addHabit() {
+    const input = document.getElementById("habitname") as HTMLInputElement
+    const habitname = input.value.trim()
+    const newHabit: habit = {
+        id: habits.length + 1,
+        name: habitname,
+        completed: false
+    }
+    habits.push(newHabit)
+    input.value = ""
+    fetchHabits()
+}
+
+fetchHabits()
